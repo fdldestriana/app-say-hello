925259641be7d36396d6a7c27e50dfc99d45be1e
fadlydestriana <fadlydestriana@gmail.com>
2023-07-14 21:56:40 +0700

    updating main function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fdldestriana/app-say-hello
 
 go 1.20
 
-require github.com/fdldestriana/go-say-hello v1.5.0 
+require github.com/fdldestriana/go-say-hello/v2 v2.0.0 
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/fdldestriana/go-say-hello"
+	go_say_hello "github.com/fdldestriana/go-say-hello/v2"
 )
 
 func main() {
-	var helloMessage string = go_say_hello.SayHello()
+	var helloMessage string = go_say_hello.SayHello("Eko")
 	fmt.Println(helloMessage)
 }
